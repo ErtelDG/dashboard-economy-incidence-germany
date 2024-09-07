6fd4a3c6097041829fe8a2205149b9751e8a2e06
ErtelDG <ertel.developer@t-online.de>
2024-09-07 20:45:42 +0200

    1725731142

--- push.ps1
+++ push.ps1
@@ -3,3 +3,12 @@ $timestamp = [int]((Get-Date).ToUniversalTime() - [datetime]"1970-01-01T00:00:00
 
 # Add all changes to git
 git add .
+
+# Commit with the current Unix time as the message
+git commit -m "$($timestamp)"
+
+# Push the changes to the remote repository
+git push
+
+# Output success message
+Write-Output "Pushed to repository with commit message: $timestamp"
